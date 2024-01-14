a5006ad208cff6407ca724e8a298929ee025def7
NTZK <127354535+nottzack@users.noreply.github.com>
2024-01-14 16:06:42 +0100

    Update lockdoors_s.lua

--- lockdoors_s.lua
+++ lockdoors_s.lua
@@ -1,6 +1,7 @@
-
+-- lockdoors_s.lua
 
 local lockedDoors = {}
+local discordWebhook = 'TU_URL_DEL_WEBHOOK'
 
 RegisterServerEvent('lockdoors:toggleLock')
 AddEventHandler('lockdoors:toggleLock', function(doorId)
@@ -10,11 +11,19 @@ AddEventHandler('lockdoors:toggleLock', function(doorId)
         lockedDoors[doorId] = source
         TriggerClientEvent('lockdoors:toggleLock', -1, doorId, true)
         TriggerClientEvent('chatMessage', source, 'Sistema de bloqueo', {255, 255, 0}, 'Puerta ' .. doorId .. ' cerrada.')
+
+        SendDiscordNotification('Puerta ' .. doorId .. ' cerrada por el jugador ' .. GetPlayerName(source))
     elseif lockedDoors[doorId] == source then
         lockedDoors[doorId] = nil
         TriggerClientEvent('lockdoors:toggleLock', -1, doorId, false)
         TriggerClientEvent('chatMessage', source, 'Sistema de bloqueo', {255, 255, 0}, 'Puerta ' .. doorId .. ' abierta.')
+
+        SendDiscordNotification('Puerta ' .. doorId .. ' abierta por el jugador ' .. GetPlayerName(source))
     else
         TriggerClientEvent('chatMessage', source, 'Sistema de bloqueo', {255, 0, 0}, 'Esta puerta ya está cerrada por otro jugador.')
     end
 end)
+
+function SendDiscordNotification(message)
+    PerformHttpRequest(discordWebhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+end
